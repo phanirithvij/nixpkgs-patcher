4f4807863143ed9f507d4563c84281baac18decb
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-05-24 01:48:10 +0200

    feat: get system from modules, remove extra args

--- flake.nix
+++ flake.nix
@@ -3,11 +3,13 @@
 
   outputs = _: {
     lib.nixosSystem =
-      # TODO: don't hardcode system
-      # maybe try to wrap it in a function (that is used to replace nixosSystem)
-      # and get nixpkgs.hostPlatform from the config?
-      { system, inputs }:
+      { specialArgs, ... }@args:
       let
+        inputs = specialArgs;
+
+        oldNixosSystem = import "${inputs.nixpkgs}/nixos/lib/eval-config.nix" args;
+        system = oldNixosSystem.config.nixpkgs.hostPlatform.system;
+
         pkgs = import inputs.nixpkgs { inherit system; };
         # take "nixpkgs" input as a base and apply patches that start with "nixpkgs-patch"
         patches = builtins.attrValues (
@@ -20,7 +22,7 @@
         };
         # don't use the patchedNixpkgs without patches, it takes time to build it
         finalNixpkgs = if patches == [ ] then inputs.nixpkgs else patchedNixpkgs;
-        nixosSystem = import "${finalNixpkgs}/nixos/lib/eval-config.nix";
+        nixosSystem = import "${finalNixpkgs}/nixos/lib/eval-config.nix" args;
       in
       nixosSystem;
   };
