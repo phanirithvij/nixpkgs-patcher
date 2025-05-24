7eb219842c3f8d9af85ee554b3cdc0586a030328
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-05-24 11:52:29 +0200

    feat: configurable patches from your module

--- flake.nix
+++ flake.nix
@@ -20,9 +20,45 @@
             system = null;
             modules = args.modules ++ [
               (
-                { ... }:
+                { lib, ... }:
+
+                let
+                  inherit (lib)
+                    mkOption
+                    mkEnableOption
+                    literalExpression
+                    types
+                    ;
+                in
                 {
                   # TODO: set config.nixpkgs.flake.source
+
+                  options.nixpkgs-patcher = {
+                    enable = mkEnableOption "nixpkgs-patcher";
+                    settings = mkOption {
+                      type = types.submodule {
+                        options = {
+                          patches = lib.mkOption {
+                            type = types.listOf types.package;
+                            default = [ ];
+                            example = literalExpression ''
+                              [
+                                (pkgs.fetchpatch2 {
+                                  name = "foo-module-init.patch";
+                                  url = "https://github.com/NixOS/nixpkgs/compare/pull/123456/head~1...pull/123456/head.patch";
+                                  hash = "";
+                                })
+                              ]
+                            '';
+                            description = ''
+                              A list of patches to apply to the nixpkgs source.
+                            '';
+                          };
+                        };
+                      };
+                      default = { };
+                    };
+                  };
                 }
               )
             ];
@@ -42,23 +78,23 @@
             or (die "Couldn't find your base nixpkgs. You need to pass the nixosSystem function an attrset with `nixpkgsPatcher.nixpkgs = inputs.nixpkgs` or name your main nixpkgs input `nixpkgs` and pass `specialArgs = inputs`.");
         patchInputRegex = config.patchInputRegex or "^nixpkgs-patch-.*";
         systemInput = config.system or args'.system;
-        patchesFromConfig = config.patches or args.patches or [ ];
-
-        system =
-          if systemInput != null then
-            systemInput
-          else
-            (import "${nixpkgs}/nixos/lib/eval-config.nix" args').config.nixpkgs.hostPlatform.system;
-
-        pkgs = import nixpkgs { inherit system; };
+        patchesFromConfig = config.patches or args.patches or (_: [ ]);
 
         inherit (nixpkgs.lib)
           filterAttrs
           ;
 
+        evaledModules = import "${nixpkgs}/nixos/lib/eval-config.nix" args';
+        system =
+          if systemInput != null then systemInput else evaledModules.config.nixpkgs.hostPlatform.system;
+        pkgs = import nixpkgs { inherit system; };
+
+        moduleConfig = evaledModules.config.nixpkgs-patcher;
+        patchesFromModules = if moduleConfig.enable then moduleConfig.settings.patches else [ ];
+
         patchesFromFlakeInputs = attrValues (filterAttrs (n: v: match patchInputRegex n != null) inputs);
 
-        patches = (patchesFromConfig pkgs) ++ patchesFromFlakeInputs;
+        patches = (patchesFromConfig pkgs) ++ patchesFromFlakeInputs ++ patchesFromModules;
         patchedNixpkgs = pkgs.applyPatches {
           # TODO: add more metadata
           name = "nixpkgs-patched";
