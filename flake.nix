b6a230d32cd8c3864acf8a548ff948d1e56f950a
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-05-24 02:06:42 +0200

    feat: make configurable, add error messages

--- flake.nix
+++ flake.nix
@@ -3,26 +3,41 @@
 
   outputs = _: {
     lib.nixosSystem =
-      { specialArgs, ... }@args:
+      args:
       let
-        inputs = specialArgs;
+        die = msg: throw "[nixpkgs-patcher]: ${msg}";
 
-        oldNixosSystem = import "${inputs.nixpkgs}/nixos/lib/eval-config.nix" args;
-        system = oldNixosSystem.config.nixpkgs.hostPlatform.system;
+        config = args.nixpkgsPatcher or { };
+        inputs =
+          config.inputs or args.specialArgs
+            or (die "Couldn't find your flake inputs. You need to pass the nixosSystem function an attrset with `nixpkgsPatcher.inputs = inputs` or `specialArgs = inputs`.");
+        nixpkgs =
+          config.nixpkgs or inputs.nixpkgs
+            or (die "Couldn't find your base nixpkgs. You need to pass the nixosSystem function an attrset with `nixpkgsPatcher.nixpkgs = inputs.nixpkgs` or name your main nixpkgs input `nixpkgs` and pass `specialArgs = inputs`.");
+        patchInputRegex = config.patchInputRegex or "^nixpkgs-patch-.*";
+        systemInput = config.system or args.system or null;
 
-        pkgs = import inputs.nixpkgs { inherit system; };
+        system =
+          if systemInput != null then
+            systemInput
+          else
+            (import "${nixpkgs}/nixos/lib/eval-config.nix" args).config.nixpkgs.hostPlatform.system;
+
+        pkgs = import nixpkgs { inherit system; };
         # take "nixpkgs" input as a base and apply patches that start with "nixpkgs-patch"
         patches = builtins.attrValues (
-          inputs.nixpkgs.lib.filterAttrs (n: v: builtins.match "^nixpkgs-patch-.*" n != null) inputs
+          nixpkgs.lib.filterAttrs (n: v: builtins.match patchInputRegex n != null) inputs
         );
         patchedNixpkgs = pkgs.applyPatches {
           name = "nixpkgs-patched";
-          src = inputs.nixpkgs;
+          src = nixpkgs;
           inherit patches;
         };
         # don't use the patchedNixpkgs without patches, it takes time to build it
-        finalNixpkgs = if patches == [ ] then inputs.nixpkgs else patchedNixpkgs;
-        nixosSystem = import "${finalNixpkgs}/nixos/lib/eval-config.nix" args;
+        finalNixpkgs = if patches == [ ] then nixpkgs else patchedNixpkgs;
+
+        args' = builtins.removeAttrs args [ "nixpkgsPatcher" ];
+        nixosSystem = import "${finalNixpkgs}/nixos/lib/eval-config.nix" args';
       in
       nixosSystem;
   };
