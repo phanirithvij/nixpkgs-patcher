2c083719ffe9c15586d2c9d6bfd99a2d65e6501a
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-06-08 13:31:07 +0200

    refactor: new lix alias

--- tests/run-all.sh
+++ tests/run-all.sh
@@ -4,6 +4,6 @@ for testDir in */; do
   pushd $testDir
   # TODO: figure out why it's broken with cppnix
   #nix run github:NixOS/nixpkgs#nixVersions.stable -- flake check
-  nix run github:NixOS/nixpkgs#lixVersions.stable -- flake check
+  nix run github:NixOS/nixpkgs#lixPackageSets.stable.lix -- flake check
   popd
 done
